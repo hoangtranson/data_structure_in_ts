c5ec5d66ce78145bd2dc34635134cefb4d7afebf
hoangtranson <son.hoang01@gmail.com>
2023-05-16 19:06:14 +0700

    add queue

--- src/queue/queue.test.ts
+++ src/queue/queue.test.ts
@@ -1,45 +1,72 @@
 import { Queue } from "./queue";
 
-test("queue length varies according to enqueued items", () => {
-  const queue = new Queue();
-  expect(queue.getLength()).toBe(0);
+describe("test functionality", () => {
+  test("queue length varies according to enqueued items", () => {
+    const queue = new Queue();
+    expect(queue.getLength()).toBe(0);
 
-  const item = { foo: "bar" };
-  queue.enqueue(item);
-  expect(queue.getLength()).toBe(1);
+    const item = { foo: "bar" };
+    queue.enqueue(item);
+    expect(queue.getLength()).toBe(1);
 
-  queue.dequeue();
-  expect(queue.getLength()).toBe(0);
+    queue.dequeue();
+    expect(queue.getLength()).toBe(0);
+  });
+
+  test("first enqueued is first dequeued", () => {
+    const queue = new Queue();
+    const itemA = { foo: "bar" };
+    const itemB = { x: "y" };
+
+    queue.enqueue(itemA);
+    queue.enqueue(itemB);
+
+    expect(queue.dequeue()).toBe(itemA);
+    expect(queue.dequeue()).toBe(itemB);
+  });
+
+  test("queue is empty when all elements were dequeued", () => {
+    const queue = new Queue();
+
+    queue.enqueue("a").enqueue("b").enqueue("c");
+
+    queue.dequeue();
+    queue.dequeue();
+    queue.dequeue();
+
+    expect(queue.isEmpty()).toBe(true);
+  });
+
+  test("items can be enqueued", () => {
+    const queue = new Queue();
+
+    queue.enqueue("a").enqueue("b").enqueue("c");
+
+    expect(queue.toString()).toBe("a,b,c");
+  });
 });
 
-test("first enqueued is first dequeued", () => {
-  const queue = new Queue();
-  const itemA = { foo: "bar" };
-  const itemB = { x: "y" };
+describe("test functionality", () => {
+  let queue: Queue<string>;
 
-  queue.enqueue(itemA);
-  queue.enqueue(itemB);
+  beforeEach(() => {
+    queue = new Queue();
 
-  expect(queue.dequeue()).toBe(itemA);
-  expect(queue.dequeue()).toBe(itemB);
-});
+    for (let i = 0; i < 50000; i++) {
+      queue.enqueue("item number" + i);
+    }
+  });
 
-test("queue is empty when all elements were dequeued", () => {
-  const queue = new Queue();
+  test("50000 element", () => {
+    expect(queue.getLength()).toBe(50000);
 
-  queue.enqueue("a").enqueue("b").enqueue("c");
+    const start = performance.now();
+    for (let i = 0; i < 50000; i++) {
+      queue.dequeue();
+    }
+    const end = performance.now();
+    console.log(`Execution time: ${end - start} ms`);
 
-  queue.dequeue();
-  queue.dequeue();
-  queue.dequeue();
-
-  expect(queue.isEmpty()).toBe(true);
-});
-
-test("items can be enqueued", () => {
-  const queue = new Queue();
-
-  queue.enqueue("a").enqueue("b").enqueue("c");
-
-  expect(queue.toString()).toBe("a,b,c");
+    expect(end - start).toBeLessThan(2000);
+  });
 });
