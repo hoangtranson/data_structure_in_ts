204e19e4adab8dbedd6d849cbe4bde28c86e0986
hoangtranson <son.hoang01@gmail.com>
2023-05-18 14:04:34 +0700

    add logic for hashtable and unit test

--- src/hash-table/hash-table.test.ts
+++ src/hash-table/hash-table.test.ts
@@ -9,4 +9,45 @@ describe("test functionality", () => {
     const biggerHashTable = new HashTable<number>(64);
     expect(biggerHashTable.value.length).toBe(64);
   });
+
+  it("should generate proper hash for specified keys", () => {
+    const hashTable = new HashTable<string>();
+
+    expect(hashTable.hash("a")).toBe(1);
+    expect(hashTable.hash("b")).toBe(2);
+    expect(hashTable.hash("abc")).toBe(6);
+  });
+
+  it("should set, read and delete data with collisions", () => {
+    const hashTable = new HashTable<string>(3);
+
+    expect(hashTable.hash("a")).toBe(1);
+    expect(hashTable.hash("b")).toBe(2);
+    expect(hashTable.hash("c")).toBe(0);
+    expect(hashTable.hash("d")).toBe(1);
+
+    hashTable.set("a", "sky-old");
+    hashTable.set("a", "sky");
+    hashTable.set("b", "sea");
+    hashTable.set("c", "earth");
+    hashTable.set("d", "ocean");
+
+    expect(hashTable.has("x")).toBe(false);
+    expect(hashTable.has("b")).toBe(true);
+    expect(hashTable.has("c")).toBe(true);
+
+    expect(hashTable.get("a")).toBe("sky");
+    expect(hashTable.get("d")).toBe("ocean");
+    expect(hashTable.get("x")).not.toBeDefined();
+
+    hashTable.delete("a");
+
+    expect(() => hashTable.delete("not-existing")).toThrow("key not correct");
+
+    expect(hashTable.get("a")).not.toBeDefined();
+    expect(hashTable.get("d")).toBe("ocean");
+
+    hashTable.set("d", "ocean-new");
+    expect(hashTable.get("d")).toBe("ocean-new");
+  });
 });
--- src/hash-table/hash-table.ts
+++ src/hash-table/hash-table.ts
@@ -1,13 +1,81 @@
+import { error } from "console";
+
 export class HashTable<T> {
-  private bucket: Array<Array<T>>;
+  private buckets: Array<Array<{ key: string; value: T }>>;
   private keys: { [key: string]: unknown };
 
   constructor(bucketLength: number = 32) {
-    this.bucket = new Array<Array<T>>(bucketLength);
+    this.buckets = new Array<Array<{ key: string; value: T }>>(bucketLength);
     this.keys = {};
   }
 
-  public get value(): Array<Array<T>> {
-    return this.bucket;
+  public get value(): Array<Array<{ key: string; value: T }>> {
+    return this.buckets;
+  }
+
+  public getKeys(): string[] {
+    return Object.keys(this.keys);
+  }
+
+  public hash(key: string): number {
+    return (
+      key
+        .split("")
+        .map((k) => k.charCodeAt(0))
+        .reduce((sum, v) => sum + v, 0) % this.buckets.length
+    );
+  }
+
+  public has(key: string): boolean {
+    return Object.hasOwnProperty.call(this.keys, key);
+  }
+
+  public set(key: string, value: T) {
+    const hash = this.hash(key);
+
+    if (this.has(key)) {
+      this.buckets[hash] = this.buckets[hash].map((item) => {
+        if (item.key === key) {
+          return {
+            key,
+            value,
+          };
+        }
+
+        return item;
+      });
+    } else {
+      if (!this.buckets[hash]) {
+        this.buckets[hash] = [];
+      }
+
+      this.buckets[hash].push({ key, value });
+    }
+
+    this.keys[key] = hash;
+  }
+
+  public get(key: string): T | undefined {
+    const hash = this.hash(key);
+    const bucket = this.buckets[hash];
+    return bucket?.find((v) => v.key === key)?.value;
+  }
+
+  public delete(key: string): void {
+    if (!this.has(key)) {
+      throw new Error("key not correct");
+    }
+
+    const hash = this.hash(key);
+    const bucket = this.buckets[hash];
+
+    if (!bucket) {
+      throw new Error("bucket no value");
+    }
+
+    this.buckets[hash] = [
+      ...this.buckets[hash].filter((item) => item.key !== key),
+    ];
+    delete this.keys[key];
   }
 }
